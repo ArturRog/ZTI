cd73ffa556ad6ec8d21ebc027e6caf9622373786
Artur <artur.rog95@gmail.com>
2018-08-24 15:29:13 +0200

    ElephantSQL

--- src/main/java/com/artur/zti/user/controller/LoginController.java
+++ src/main/java/com/artur/zti/user/controller/LoginController.java
@@ -18,9 +18,9 @@ public class LoginController {
     public String login() {
 
         //todo delete this
-        if (userService.findByUsername("a")!=null)
-            return "login";
-        userService.save(new User("a", "a", "a", "a@a", UserRole.ADMIN.getRole()));
+//        if (userService.findByUsername("a")!=null)
+//            return "login";
+//        userService.save(new User("a", "a", "a", "a@a", UserRole.ADMIN.getRole()));
         return "login";
     }
 
